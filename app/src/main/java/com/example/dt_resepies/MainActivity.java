2f67ecc958bc875a5ffd14862b08e906d568add9
Jack0nail <kirsid2000@mail.ru>
2021-05-02 23:14:52 +0300

    Дописать парсер разобраться с классом Save_json составить тестовый json

--- app/src/main/java/com/example/dt_resepies/MainActivity.java
+++ app/src/main/java/com/example/dt_resepies/MainActivity.java
@@ -2,12 +2,24 @@ package com.example.dt_resepies;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import org.json.JSONArray;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    JSONArray jsonArray;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void viewRecipes(View view){
+        setContentView(R.layout.recipe_list);
+        listView = findViewById(R.id.recipes_list);
+        jsonArray = (jsonArray) ;// дописать парсер написать тестовый json
     }
 }
---
+++ app/src/main/java/com/example/dt_resepies/Save_json.java
@@ -0,0 +1,74 @@
+package com.example.dt_resepies;
+
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetManager;
+
+import org.json.JSONArray;
+//import org.json.simple.parser.JSONParser;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+import static android.content.Context.MODE_PRIVATE;
+
+class Save_json {
+//    public void writeFile(JSONArray listView, Context context, String file_name) {
+//        try {
+//            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(context.openFileOutput(file_name, MODE_PRIVATE)));
+//            bw.write(listView.toJSONString());
+//            bw.close();
+//            System.out.println("zalypa zapisalas");
+//        } catch (
+//                IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
+
+    private static String convertStreamToString(InputStreamReader is) {
+
+        BufferedReader reader = new BufferedReader(is);
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+    }
+
+    public String getAssetsfile(String file_name, Activity activity) throws IOException {
+        AssetManager am = activity.getAssets();
+        InputStream is = am.open(file_name);
+        String s = convertStreamToString(new InputStreamReader(is));
+        is.close();
+        System.out.println(s);
+        return s;
+    }
+
+//    public JSONArray readFile(Context context, String file_name) {
+//        JSONArray jsonstr = new JSONArray();
+//        try {
+//            jsonstr = (JSONArray) new JSONParser().parse(convertStreamToString(new InputStreamReader(context.openFileInput(file_name))));
+//        } catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//        return jsonstr;
+    }
+//}
